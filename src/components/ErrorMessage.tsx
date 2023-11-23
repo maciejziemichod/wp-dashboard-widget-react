e9e6aa77612d4e49be0c13ced66618a5220454f3
maciejziemichod <maciej.ziemichod@gmail.com>
2023-11-23 23:32:24 +0100

    feat: add error message component

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+import styles from "./ErrorMessage.module.css";
+
+type ErrorMessageProps = {
+	message: string;
+};
+
+export function ErrorMessage({ message }: ErrorMessageProps) {
+	return <p className={styles.message}>{message}</p>;
+}
