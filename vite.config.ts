f0f7fe887636890ae4e14280b8a24a65695d7069
maciejziemichod <maciej.ziemichod@gmail.com>
2023-11-24 23:04:21 +0100

    build: plugin assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "admin/build",
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
-  },
+	plugins: [react()],
+	build: {
+		outDir: 'admin/build',
+		rollupOptions: {
+			output: {
+				entryFileNames: `[name].js`,
+				chunkFileNames: `[name].js`,
+				assetFileNames: `[name].[ext]`,
+			},
+		},
+	},
 });
